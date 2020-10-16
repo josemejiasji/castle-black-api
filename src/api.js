b16deb329afbe45f8dcecf0892dce7d5df7d96ac
Jose <josemejiasji@gmail.com>
2020-10-16 18:11:36 +0200

    added create player endpoint with test

--- src/api.js
+++ src/api.js
@@ -12,4 +12,12 @@ api.get("/players", function(req, res) {
   res.json(players);
 });
 
+api.post("/players", function(req, res) {
+  const newPlayer = req.body;
+  players.push(newPlayer);
+
+  res.status(201);
+  res.json(newPlayer);
+});
+
 module.exports = api;
--- tests/api.test.js
+++ tests/api.test.js
@@ -10,5 +10,19 @@ describe('Player endpoints', function () {
         expect(res.status).toBe(200);
         expect(res.body).toEqual(db.players);
         done();
+    });
+
+    it('should create a new player', async function (done) {
+        const newPlayer = {
+            name: 'Arya Stark',
+            age: 18,
+            health: 100,
+            bag: [],
+        };
+        const res = await request.post('/api/players').send(newPlayer);
+
+        expect(res.status).toBe(201);
+        expect(res.body).toEqual(newPlayer);
+        done();
     })
-})
+});
