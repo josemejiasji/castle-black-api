5ac523c1949b853a36ff887b20dbf62b848c6811
Jose <josemejiasji@gmail.com>
2020-10-18 13:37:35 +0200

    added players actions arm logic

--- src/services/player.js
+++ src/services/player.js
@@ -15,7 +15,21 @@ const findOne = function (id) {
 }
 
 const doAction = function (id, action, payload) {
-    throw "Not implemented";
+    const playerIndex = players.findIndex((player) => player.id == id)
+
+    if (playerIndex < 0) {
+        return null;
+    }
+
+    if (action === 'kill') {
+        players[playerIndex].health = 0;
+    } else if (action === 'arm') {
+        if (players[playerIndex].bag.includes(payload)) {
+            players[playerIndex].armedObject = payload;
+        }
+    }
+
+    return players[playerIndex];
 }
 
 module.exports = {
