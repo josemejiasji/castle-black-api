c5b6c71c23c3a827903a1fe4b6a0a7361ee5b91b
Jose <josemejiasji@gmail.com>
2020-10-19 19:45:19 +0200

    answered questions from comments

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const api = require("./src/api.js");
 const app = express();
 
 app.disable("x-powered-by"); // QUESTION: any reason is this line here?
+// ANSWER: it is usually removed for security reasons. You remove this header to prevent malicious users from trying out public-known bugs of Express
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const router = Router();
 router.get("/health", function(req, res) {
   res.body = "Up and running";
   // QUESTION: why this endpoint blocks the app?
+  // ANSWER: because there is no response sent back to the user. It's setting response's body but is not finishing the request with .send() or json().
 });
 
 module.exports = router;
